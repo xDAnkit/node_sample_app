3d14c58a8f06d09f3df6a990516abf4025c98555
Ankit Jain <xDAnkitjain@gmail.com>
2022-03-10 15:29:10 +0530

    Updated main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World from Jenkins with PM2" });
+  res.send({ message: "Welcome to VSS India" });
 });
 
 app.listen(port, () => {

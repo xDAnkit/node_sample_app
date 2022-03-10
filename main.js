2ba0b0c332c49595841aab742291913e07ecfe70
Ankit Jain <xDAnkitjain@gmail.com>
2022-03-10 15:38:36 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send({ message: "Welcome to VSS India" });
+  res.send({ message: "Triggered using Webhooks" });
 });
 
 app.listen(port, () => {
